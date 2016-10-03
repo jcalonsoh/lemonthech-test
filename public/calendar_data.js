bf15f16a73dd90a2d5388eef4b2e8af81a782931
Juan Carlos Alonso Holmstron <j.carlos.alonso.h@outlook.cl>
2016-10-03 01:49:17 -0300

    Reviewing data calendar

--- public/calendar_data.js
+++ public/calendar_data.js
@@ -4,7 +4,8 @@ var citas = {
         {nombre: 'Daniel', hora_inicio: '09:30', hora_termino: '11:00'},
         {nombre: 'Daniel', hora_inicio: '15:00', hora_termino: '16:00'},
         {nombre: 'Daniel', hora_inicio: '17:00', hora_termino: '19:30'}
-    ], martes: [
+    ],
+    martes: [
         {nombre: 'Daniel', hora_inicio: '08:00', hora_termino: '09:00'},
         {nombre: 'Daniel', hora_inicio: '11:30', hora_termino: '12:00'},
         {nombre: 'Daniel', hora_inicio: '15:00', hora_termino: '16:00'},
@@ -15,15 +16,17 @@ var citas = {
         {nombre: 'Daniel', hora_inicio: '10:30', hora_termino: '12:00'},
         {nombre: 'Daniel', hora_inicio: '15:00', hora_termino: '16:00'},
         {nombre: 'Daniel', hora_inicio: '17:00', hora_termino: '19:30'}
-    ], jueves: [
-        {nombre: 'Daniel', hora_inicio: '08:00', hora_termino: '09:00'},
-        {nombre: 'Daniel', hora_inicio: '09:30', hora_termino: '12:00'},
-        {nombre: 'Daniel', hora_inicio: '15:00', hora_termino: '16:00'},
-        {nombre: 'Daniel', hora_inicio: '17:00', hora_termino: '19:30'}
-    ], viernes: [
+    ],
+    jueves: [
         {nombre: 'Daniel', hora_inicio: '08:00', hora_termino: '09:00'},
         {nombre: 'Daniel', hora_inicio: '09:30', hora_termino: '12:00'},
         {nombre: 'Daniel', hora_inicio: '15:00', hora_termino: '16:00'},
         {nombre: 'Daniel', hora_inicio: '17:00', hora_termino: '19:30'}
     ],
+    viernes: [
+        {nombre: 'Daniel', hora_inicio: '08:00', hora_termino: '09:00'},
+        {nombre: 'Daniel', hora_inicio: '09:30', hora_termino: '12:00'},
+        {nombre: 'Daniel', hora_inicio: '15:00', hora_termino: '16:00'},
+        {nombre: 'Daniel', hora_inicio: '17:00', hora_termino: '19:30'}
+    ]
 }
